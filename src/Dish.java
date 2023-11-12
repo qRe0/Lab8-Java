fda0cf365dd6a2b97caa33aec4bd66c9e99c0f87
qRe <melsenmalina@gmail.com>
2023-11-12 20:51:56 +0300

    totalPrice

--- src/Dish.java
+++ src/Dish.java
@@ -18,3 +18,5 @@ public class Dish implements Serializable {
         return price;
     }
 }
+
+//
--- src/FoodOrderClient.java
+++ src/FoodOrderClient.java
@@ -64,3 +64,5 @@ public class FoodOrderClient {
         return scanner.nextLine();
     }
 }
+
+//
