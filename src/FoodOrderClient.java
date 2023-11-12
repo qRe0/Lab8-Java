102d31c3baa1541b7439a1358262e1e08328d68c
qRe <melsenmalina@gmail.com>
2023-11-12 12:42:35 +0300

    clientPart - ready

--- src/FoodOrderClient.java
+++ src/FoodOrderClient.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class FoodOrderClient {
     public static void main(String[] args) {
         try (
-                Socket socket = new Socket("localhost", 12345);
+                Socket socket = new Socket("192.168.0.107", 12345);
                 ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
                 ObjectInputStream in = new ObjectInputStream(socket.getInputStream())
         ) {
